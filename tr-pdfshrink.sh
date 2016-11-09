46a2c993bc7ab076a2d153aa2e10b438d4d45c93
trasba <t.rasbach@gmx.net>
2016-11-09 11:45:50 +0100

    Create tr-pdfshrink.sh

---
+++ tr-pdfshrink.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#tr-pdfshrink commandline to shrink pdf files by compressing images & document to 96dpi
+
+temp=${1,,}
+if test ! "${temp##*.}" = "pdf"
+then
+        echo "The file extension must be .pdf"
+        exit
+fi
+gs -sDEVICE=pdfwrite \
+-dDownsampleColorImages=true \
+-dDownsampleGrayImages=true \
+-dDownsampleMonoImages=true \
+-dColorImageResolution=96 \
+-dGrayImageResolution=96 \
+-dMonoImageResolution=96 \
+-dColorImageDownsampleThreshold=1.0 \
+-dGrayImageDownsampleThreshold=1.0 \
+-dMonoImageDownsampleThreshold=1.0 \
+-dCompatibilityLevel=1.4 \
+-dPDFSETTINGS=/ebook \
+-dNOPAUSE \
+-dQUIET \
+-dBATCH \
+-sOutputFile=${1%.*}_small.pdf ${1}
+
+echo "The file " ${1} " was converted."
